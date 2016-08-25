ba5278e0b84cad9266158499904c24e15b3a9632
Kevin <411190068@qq.com>
2016-08-25 21:18:11 +0800

    remove the catch exception block

--- src/main/java/com/kevin/util/Base64FileUtil.java
+++ src/main/java/com/kevin/util/Base64FileUtil.java
@@ -13,58 +13,58 @@ import java.util.Base64;
  * <p>
  * Created by Kevin on 2016/8/20.<br/>
  * Last Update Time 2016/8/22.
- * @since 1.8
+ *
  * @see java.util.Base64
+ * @since 1.8
  */
 public class Base64FileUtil {
 
     /**
      * Convert file to Base64 string
+     *
      * @param filePath the path of the file
      * @return Base64 String
+     * @throws IOException
      */
-	public static String fileToBase64(String filePath) throws IOException {
-		byte[] data = null;
+    public static String fileToBase64(String filePath) throws IOException {
+        byte[] data = null;
         InputStream in = null;
-		try {
-			in = new FileInputStream(filePath);
-			data = new byte[in.available()];
-			in.read(data);
-		} catch (IOException e) {
-			throw e;
-		} finally {
+        try {
+            in = new FileInputStream(filePath);
+            data = new byte[in.available()];
+            in.read(data);
+        } finally {
             if (in != null) {
                 in.close();
             }
         }
         return encode(data);
-	}
+    }
 
     /**
      * Convert Base64 string to file
-     * @param base64 Base64 string
+     *
+     * @param base64   Base64 string
      * @param savePath the save path of the file converts from Base64 string
+     * @throws IOException
      */
-	public static void base64ToFile(String base64,String savePath) throws IOException {
+    public static void base64ToFile(String base64, String savePath) throws IOException {
         byte[] bytes = decode(base64);
         FileOutputStream out = null;
         try {
             out = new FileOutputStream(savePath);
             out.write(bytes);
-
-        } catch (IOException e) {
-            throw e;
         } finally {
             if (out != null) {
                 out.close();
             }
         }
     }
-	
-	public static String encode(byte[] data) {
+
+    public static String encode(byte[] data) {
         return Base64.getEncoder().encodeToString(data);
     }
-	
+
     public static byte[] decode(String base64) {
         return Base64.getDecoder().decode(base64);
     }
--- src/main/java/com/kevin/util/Base64Util.java
+++ src/main/java/com/kevin/util/Base64Util.java
@@ -11,58 +11,58 @@ import javax.xml.bind.DatatypeConverter;
  * <p>
  * Created by Kevin on 2016/8/20.<br/>
  * Last Update Time 2016/8/22.
- * @since 1.6
+ *
  * @see javax.xml.bind.DatatypeConverter
+ * @since 1.6
  */
 public class Base64Util {
 
     /**
      * Convert file to Base64 String
+     *
      * @param filePath the path of the file
      * @return Base64 String
+     * @throws IOException
      */
-	public static String fileToBase64(String filePath) throws IOException {
-		byte[] data = null;
+    public static String fileToBase64(String filePath) throws IOException {
+        byte[] data = null;
         InputStream in = null;
-		try {
+        try {
             in = new FileInputStream(filePath);
-			data = new byte[in.available()];
-			in.read(data);
-		} catch (IOException e) {
-			throw e;
-		} finally {
-		    if (in !=null) {
+            data = new byte[in.available()];
+            in.read(data);
+        } finally {
+            if (in != null) {
                 in.close();
             }
         }
         return encode(data);
-	}
+    }
 
     /**
      * Convert Base64 string to file
-     * @param base64 Base64 string
+     *
+     * @param base64   Base64 string
      * @param savePath the save path of the file converts from Base64 string
+     * @throws IOException
      */
-	public static void base64ToFile(String base64,String savePath) throws IOException {
+    public static void base64ToFile(String base64, String savePath) throws IOException {
         byte[] bytes = decode(base64);
         FileOutputStream out = null;
         try {
             out = new FileOutputStream(savePath);
             out.write(bytes);
-            out.close();
-        } catch (IOException e) {
-            throw e;
         } finally {
             if (out != null) {
                 out.close();
             }
         }
     }
-	
-	public static String encode(byte[] data) {
+
+    public static String encode(byte[] data) {
         return DatatypeConverter.printBase64Binary(data);
     }
-	
+
     public static byte[] decode(String base64) {
         return DatatypeConverter.parseBase64Binary(base64);
     }
